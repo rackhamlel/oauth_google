eaa63d063886f52547b5f089f27af7d085ecf16c
maxime <maxime@SLAM2-192.fenelonsio.local>
2024-03-04 14:45:58 +0100

    test1

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+define('GOOGLE_ID', '372301446734-k8cp15to4hfcnlngo824u05r6oqnvplq.apps.googleusercontent.com');
+define('GOOGLE_SECRET', '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');
---
+++ connection.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> CONNECTION</h1>
+    <p>
+        <a href="
+        scope=email&
+        access_type=online&
+        
+        response_type=code&
+        
+        redirect_uri=<?php urlencode('')
+        client_id=client_id"></a>
+    </p>
+</body>
+</html>
